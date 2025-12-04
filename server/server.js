0c1bf7bc9c8f5a4eec75f2fab05869d5a370610d
iamsoumya1611 <soumyamajumder2022@gmail.com>
2025-12-04 12:26:03 +0530

    Fix static file serving path in server.js

--- server/server.js
+++ server/server.js
@@ -117,10 +117,10 @@ app.get('/health', (req, res) => {
 
 // Catch-all route for frontend
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, './client/build')));
   
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
   });
 }
 
